7db15180fddb231a9f63e2a92bd89a97dee97572
Tyler D Duprey <tylerduprey@outlook.com>
2021-09-12 17:07:59 -0400

    Added a 404 response

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.use(function (req, res, next) {
+  res.status(404).send("404 Error. Sorry can't find that!")
+});
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
